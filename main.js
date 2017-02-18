1c3a7617827c43134f194bd03b2c6e940448c35a
Ya'ar Hever <yaarhever@gmail.com>
2017-02-18 18:41:48 +0100

    show coordinates

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-$(document).ready(function () {
+$(document).ready(function() {
     "use strict";
 
     var canvas = document.getElementById('myMandala');
     var ctx = canvas.getContext("2d");
-    canvas.height = window.innerHeight-35;
-    canvas.width = window.innerHeight-35;
+    canvas.height = window.innerHeight - 35;
+    canvas.width = window.innerHeight - 35;
     var w = canvas.width;
     var h = canvas.height;
 
-    $('canvas').on('touchstart', function (e) {
+    function eventToCanvasCoords(e) {
+        return [e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop];
+    }
+
+    $('canvas').on('touchstart', function(e) {
         e.preventDefault();
         $('#console').text('touch start!');
+        var coords = eventToCanvasCoords(e);
+        $('#details').text('x = ' + coords[0] + ', y = ' + coords[1]);
     });
 
-    $('canvas').on('touchmove', function (e) {
+    $('canvas').on('touchmove', function(e) {
         e.preventDefault();
         $('#console').text('touch move!');
+        var coords = eventToCanvasCoords(e);
+        $('#details').text('x = ' + coords[0] + ', y = ' + coords[1]);
     });
 
-    $('canvas').on('touchend', function (e) {
+    $('canvas').on('touchend', function(e) {
         e.preventDefault();
         $('#console').text('touch end!');
+        var coords = eventToCanvasCoords(e);
+        $('#details').text('x = ' + coords[0] + ', y = ' + coords[1]);
     });
 
 });
