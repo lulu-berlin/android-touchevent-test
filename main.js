3a84c194e43c39c6f468b1692d9664bcfc0739b8
Ya'ar Hever <yaarhever@gmail.com>
2017-02-18 18:23:17 +0100

    add canvas code

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 (function () {
     "use strict";
 
-    $('#myMandala').on('touchstart', function (e) {
+    var canvas = document.getElementById('myMandala');
+    var ctx = canvas.getContext("2d");
+    canvas.height = window.innerHeight-35;
+    canvas.width = window.innerHeight-35;
+    var w = canvas.width;
+    var h = canvas.height;
+
+    $('canvas').on('touchstart', function (e) {
         e.preventDefault();
         $('#console').text('touch start!');
 
